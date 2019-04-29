73caf37f4c7b7fea136cb92a9627d9b0e1ac2b00
Armin Sabouri <asabouri@normatecusa.com>
2019-04-29 15:41:32 -0800

    Added I2C Commands from data sheet

--- Fat_Pants_Firmware/Fat Pants Test/main.c
+++ Fat_Pants_Firmware/Fat Pants Test/main.c
@@ -47,6 +47,46 @@
 	#error "This firmware only supports either the PIC18F4550 or PIC18F2550 microcontrollers."
 #endif
 
+#define BLACK                          0 ///< Draw 'off' pixels
+#define WHITE                          1 ///< Draw 'on' pixels
+#define INVERSE                        2 ///< Invert pixels
+
+#define SSD1306_MEMORYMODE          0x20 ///< See datasheet
+#define SSD1306_COLUMNADDR          0x21 ///< See datasheet
+#define SSD1306_PAGEADDR            0x22 ///< See datasheet
+#define SSD1306_SETCONTRAST         0x81 ///< See datasheet
+#define SSD1306_CHARGEPUMP          0x8D ///< See datasheet
+#define SSD1306_SEGREMAP            0xA0 ///< See datasheet
+#define SSD1306_DISPLAYALLON_RESUME 0xA4 ///< See datasheet
+#define SSD1306_DISPLAYALLON        0xA5 ///< Not currently used
+#define SSD1306_NORMALDISPLAY       0xA6 ///< See datasheet
+#define SSD1306_INVERTDISPLAY       0xA7 ///< See datasheet
+#define SSD1306_SETMULTIPLEX        0xA8 ///< See datasheet
+#define SSD1306_DISPLAYOFF          0xAE ///< See datasheet
+#define SSD1306_DISPLAYON           0xAF ///< See datasheet
+#define SSD1306_COMSCANINC          0xC0 ///< Not currently used
+#define SSD1306_COMSCANDEC          0xC8 ///< See datasheet
+#define SSD1306_SETDISPLAYOFFSET    0xD3 ///< See datasheet
+#define SSD1306_SETDISPLAYCLOCKDIV  0xD5 ///< See datasheet
+#define SSD1306_SETPRECHARGE        0xD9 ///< See datasheet
+#define SSD1306_SETCOMPINS          0xDA ///< See datasheet
+#define SSD1306_SETVCOMDETECT       0xDB ///< See datasheet
+
+#define SSD1306_SETLOWCOLUMN        0x00 ///< Not currently used
+#define SSD1306_SETHIGHCOLUMN       0x10 ///< Not currently used
+#define SSD1306_SETSTARTLINE        0x40 ///< See datasheet
+
+#define SSD1306_EXTERNALVCC         0x01 ///< External display voltage source
+#define SSD1306_SWITCHCAPVCC        0x02 ///< Gen. display voltage from 3.3V
+
+#define SSD1306_RIGHT_HORIZONTAL_SCROLL              0x26 ///< Init rt scroll
+#define SSD1306_LEFT_HORIZONTAL_SCROLL               0x27 ///< Init left scroll
+#define SSD1306_VERTICAL_AND_RIGHT_HORIZONTAL_SCROLL 0x29 ///< Init diag scroll
+#define SSD1306_VERTICAL_AND_LEFT_HORIZONTAL_SCROLL  0x2A ///< Init diag scroll
+#define SSD1306_DEACTIVATE_SCROLL                    0x2E ///< Stop scroll
+#define SSD1306_ACTIVATE_SCROLL                      0x2F ///< Start scroll
+#define SSD1306_SET_VERTICAL_SCROLL_AREA             0xA3 ///< Set scroll range
+
 // Define the globals for the USB data in the USB RAM of the PIC18F*550
 #pragma udata
 #pragma udata USB_VARIABLES=0x480
@@ -314,21 +354,16 @@ void main(void)
         USBDeviceAttach();
     #endif
 	
-	// Initialise the debug log functions
-//    debugInitialise();
-    
     // Show that we are up and running
     mStatusLED0_on();
-	
-//	sprintf(debugString, "USB Generic HID Demonstration 3");
-//	debugOut(debugString);
-
-//	sprintf(debugString, "(C)2011 Simon Inns - http://www.waitingforfriday.com");
-//	debugOut(debugString);
-	
-//	sprintf(debugString, "USB Device Initialised.");
-//	debugOut(debugString);
+	__delay_us(10);
     
+        i2c_start();
+        i2cWriteByte(0x3c);
+        i2cWriteBytes("\x00\xD3\x00\x00\x00\x40\x00\x8d", 8);
+        i2cWriteBytes("\x00\xDB\x00\x40\x00\xA4\x00\xa6\x00\x2e\x00\xAF", 12);
+        i2cStop();
+    while(1);
 	// Main processing loop
     while(1)
     {
